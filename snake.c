b2c9d05e8c4a3ba60ffc8e1feaced6c32a97673f
Heitor Caldeira <heitorcaldeira@gmail.com>
2025-01-12 22:28:10 -0300

    adding keyboard events

--- snake.c
+++ snake.c
@@ -10,6 +10,7 @@
 
 char dir = 'r';
 bool quit = false;
+int count = 3;
 int snake[WIDTH * HEIGHT][2] = {
   {10, 10},
   {11, 10},
@@ -45,23 +46,26 @@ void game_loop(SDL_Renderer* renderer) {
   }
 
   sdlc(SDL_SetRenderDrawColor(renderer, 255, 255, 18, 255));
-  int snake_size = sizeof(snake) / sizeof(int) / 2;
-  for (int i = 0; i < snake_size; i++) {
-    if (snake[i][0] == 0 && snake[i][1] == 0) {
-      continue;
-    }
-
+  for (int i = 0; i < count; i++) {
     SDL_Rect rect = { snake[i][0] * CELL_SIZE + 1, snake[i][1] * CELL_SIZE + 1, CELL_SIZE - 1, CELL_SIZE - 1 };
     sdlc(SDL_RenderFillRect(renderer, &rect));
   }
 
   if (dir == 'r') {
-    for (int i = 0; i < snake_size; i++) {
+    for (int i = 0; i < count; i++) {
       if (snake[i][0] == 0 && snake[i][1] == 0) {
         continue;
       }
 
       snake[i][0] = snake[i][0] + 1;
+    }
+  } else if (dir == 'l') {
+    for (int i = 0; i < count; i++) {
+      if (snake[i][0] == 0 && snake[i][1] == 0) {
+        continue;
+      }
+
+      snake[i][0] = snake[i][0] - 1;
     }
   }
 }
@@ -82,6 +86,20 @@ int main(void) {
       switch (event.type) {
         case SDL_QUIT:
           quit = true;
+          break;
+        case SDL_KEYDOWN:
+          {
+            SDL_Keycode code = event.key.keysym.sym;
+            if (code == 'd') {
+              if (dir != 'a') dir = 'd';
+            } else if (code == 'a') {
+              if (dir != 'd') dir = 'a';
+            } else if (code == 'w') {
+              if (dir != 's') dir = 'w';
+            } else if (code == 's') {
+              if (dir != 'w') dir = 's';
+            }
+          }
           break;
       }
     }
